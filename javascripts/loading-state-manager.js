f54fa2468858a4ed3eb56bc00c0fe6298c608bb6
LucasHitoshi <lucasogawarec@gmail.com>
2024-01-17 08:30:13 -0300

    perf: Resize about-us images to improve loading speed.

--- javascripts/loading-state-manager.js
+++ javascripts/loading-state-manager.js
@@ -1,18 +1,18 @@
-var w = window;
-var d = document;
+// var w = window;
+// var d = document;
 
-window.scrollTo(0, 0);
-disableScroll();
+// window.scrollTo(0, 0);
+// disableScroll();
 
-/** @type {HTMLDivElement} */
-const loading = document.querySelector("#page-loading");
-/** @type {HTMLButtonElement} */
-const ignoreLoadButton = document.querySelector("#ignore-load-button");
+// /** @type {HTMLDivElement} */
+// const loading = document.querySelector("#page-loading");
+// /** @type {HTMLButtonElement} */
+// const ignoreLoadButton = document.querySelector("#ignore-load-button");
 
-function endUpLoading() {
-    loading.style.display = "none";
-    enableScroll();
-}
+// function endUpLoading() {
+//     loading.style.display = "none";
+//     enableScroll();
+// }
 
-ignoreLoadButton.addEventListener("click", endUpLoading);
-w.addEventListener("load", endUpLoading);
+// ignoreLoadButton.addEventListener("click", endUpLoading);
+// w.addEventListener("load", endUpLoading);
